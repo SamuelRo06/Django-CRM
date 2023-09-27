79327447f63c73ab83b6c86283a72f9ae04a1866
Samuel Rodriguez <serodriguez0601@gmail.com>
2023-09-27 18:34:01 -0400

    Final Project

--- website/forms.py
+++ website/forms.py
@@ -2,6 +2,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+from .models import Record
+
 class SingUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
     first_name = forms.CharField(label="", max_length=50, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
@@ -9,10 +11,10 @@ class SingUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = {'username','first_name','last_name','email','password1','password2'}
+        fields = {'username','first_name','last_name','email','password2','password1'}
 
     def __init__(self, *args, **kwargs):
-        super(SignUpForm, self).__init__(*args, **kwargs)
+        super(SingUpForm, self).__init__(*args, **kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
@@ -29,3 +31,18 @@ class SingUpForm(UserCreationForm):
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
     
+# Create Add Record Form
+class AddRecordForm(forms.ModelForm):
+	first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="")
+	last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="")
+	email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Email", "class":"form-control"}), label="")
+	phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Phone", "class":"form-control"}), label="")
+	address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Address", "class":"form-control"}), label="")
+	city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"City", "class":"form-control"}), label="")
+	state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"State", "class":"form-control"}), label="")
+	zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Zipcode", "class":"form-control"}), label="")
+
+	class Meta:
+		model = Record
+		exclude = ("user",)
+
--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from website.forms import SingUpForm, AddRecordForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -16,9 +19,7 @@ def home(request):
             messages.success(request, 'There was an error logging in. Please try again.')
             return redirect('home')
     else:
-        return render(request, 'home.html')
-
-
+        return render(request, 'home.html', {'records':records})
 
 def user_logout(request):
     logout(request)
@@ -26,4 +27,65 @@ def user_logout(request):
     return redirect('home')
 
 def user_register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = SingUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password2']
+            user = authenticate(username=username, password=password) 
+            login(request, user)
+            messages.success(request, 'You have been successfully Registered!')
+            return redirect('home')
+    else:
+        form = SingUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Look Up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, 'You must be logged in to view that page.')
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, 'Record deleted successfully')
+        return redirect('home')
+    else:
+        messages.success(request, 'You must be logged in to delete')
+        return redirect('home')
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added.")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
+
+
+def update_record(request, pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record Has Been Updated!")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
